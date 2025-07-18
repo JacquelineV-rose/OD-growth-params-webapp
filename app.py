682a1f50587190d0ece5f9bf8c21665f4291190d
JacquelineV-rose <jvermette1010@gmail.com>
2025-07-17 23:28:32 -0400

    Fixed so that it reads a file with missing values

--- app.py
+++ app.py
@@ -44,7 +44,10 @@ def index():
                     transformed_data = TecanDataTransformer.transform_data(df)
                     well_data = TecanDataTransformer.get_transformed_data(transformed_data)
 
-                    plate = Wellplate((16, 24), well_data)
+                    plate_shape = Wellplate.detect_plate_layout(well_data.columns)
+                    print(f"Detected plate layout: {plate_shape}")
+                    plate = Wellplate(plate_shape, well_data)
+
 
                     #plot_path = os.path.join(app.config['RESULT_FOLDER'], f'image_{i}.png')
                     #plate.plot_raw_data(save_path=plot_path)
@@ -90,6 +93,25 @@ def dash_embed():
 
 dash_app.layout = html.Div([
     html.H2("Interactive Growth Curve Viewer"),
+
+    html.Div([
+        html.A(
+            "Return to Upload Page",
+            href="/",
+            style={
+                "display": "inline-block",
+                "padding": "8px 16px",
+                "backgroundColor": "#ffffff",
+                "color": "Black",
+                "border": "1px solid #007bff",
+                "textDecoration": "none",
+                "borderRadius": "4px",
+                "marginBottom": "20px",
+                "fontWeight": "bold"
+            }
+        )
+    ]),
+
 
     html.Label("Select wells (you can type to search):"),
     dcc.Dropdown(
